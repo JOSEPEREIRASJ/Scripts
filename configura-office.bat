b9cb95cb5ac60da12eb948dd139367b3155bd4a0
JOSEPEREIRASJ <43482327+JOSEPEREIRASJ@users.noreply.github.com>
2025-04-14 16:14:33 -0300

    Update and rename meuscript.bat to configura-office.bat

--- configura-office.bat
+++ configura-office.bat
@@ -1,3 +1,4 @@
+#Criar arquivo config
 @"
 <Configuration>
 <Add OfficeClientEdition="64" Channel="MonthlyEnterprise">
@@ -9,5 +10,9 @@
 </Configuration>
 "@ | Out-File -FilePath C:\Users\$env:USERNAME\Downloads\'config.xml'
 
-
+#Baixar Setup Office
 wget -o C:\Users\$env:USERNAME\Downloads\setup.exe https://officecdn.microsoft.com/pr/wsus/setup.exe
+
+#Instalar Office
+"Start-Process -FilePath \"C:\\Users\\TIXIT\\Downloads\\setup.exe\" -ArgumentList \"/configure C:\\Users\\TIXIT\\Downloads\\config.xml\" -Wait"' shell='cmd'
+
