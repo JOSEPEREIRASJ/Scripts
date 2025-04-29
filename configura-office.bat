6e5fbe93d697c07a1a0103fb73f2c3e7ce77e79f
JOSEPEREIRASJ <43482327+JOSEPEREIRASJ@users.noreply.github.com>
2025-04-29 16:20:52 -0300

    Update configura-office.bat

--- configura-office.bat
+++ configura-office.bat
@@ -1,18 +1,1 @@
-#Criar arquivo config
-@"
-<Configuration>
-<Add OfficeClientEdition="64" Channel="MonthlyEnterprise">
-    <Product ID="O365ProPlusRetail">
-     <Language ID="pt-br" />
-    </Product>
-</Add>
-<Display Level="Full" AcceptEULA="TRUE" />
-</Configuration>
-"@ | Out-File -FilePath C:\Users\$env:USERNAME\Downloads\'config.xml'
-
-#Baixar Setup Office
-wget -o C:\Users\$env:USERNAME\Downloads\setup.exe https://officecdn.microsoft.com/pr/wsus/setup.exe
-
-#Instalar Office
-"Start-Process -FilePath \"C:\\Users\\TIXIT\\Downloads\\setup.exe\" -ArgumentList \"/configure C:\\Users\\TIXIT\\Downloads\\config.xml\" -Wait"' shell='cmd'
-
+wget -o %HOME%\Downloads\setup.exe https://officecdn.microsoft.com/pr/wsus/setup.exe
